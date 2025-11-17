720ab4b67bb984d97ba913ca21a3ec1289efad4d
karnaksy <karnaksy@student.21-school.ru>
2025-11-17 18:43:42 +0500

    update

---
+++ intro/daytimecpcli1.c
@@ -0,0 +1,23 @@
+#include "../lib/unp.h"
+
+#define PORT 13
+
+int main(int argc, char **argv) {
+    int sockfd, n;
+    char recvline[MAXLINE + 1];
+    struct sockaddr_in servaddr;
+
+    if (argc != 2) err_quit("use: a.out <IP>");
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) err_sys("socket error");
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) err_quit("inet_pron error for %s", argv[1]);
+    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) err_sys("connect error");
+    while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
+        recvline[n] = 0;
+        if (fputs(recvline, stdout) == EOF) err_sys("fputs error");
+    }
+    if (n < 0) err_sys("read error");
+    return 0;
+}
