efa51801433b1cf0827f5f3949ec7e0c954f4e65
haihem <haihemgm@gmail.com>
2025-11-21 01:27:00 +0500

    update

---
+++ intro/ver1/cli.c
@@ -0,0 +1,12 @@
+#include "../../lib/time_server.h"
+
+int main() {
+    
+    struct sockaddr_in socket = {0};
+    //memset(&socket, 0, sizeof(socket));
+    FILE * socket_fd;
+    char buff[MAXLINE+1];
+
+    return 0;
+}
+
